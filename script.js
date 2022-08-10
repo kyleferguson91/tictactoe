dfd18196b4e72c309eeae809f8b3a0fa94ce4c4a
Kyle <kyleferguson91@gmail.com>
2022-08-10 11:26:44 -0400

    added function for playRound

--- script.js
+++ script.js
@@ -1,24 +1,62 @@
-function getComputerChoise() {
+function getComputerChoice() {
 const random = Math.ceil(Math.random()*10)
-console.log(random)
 
 
 
 
 if (random >=6) {
-    console.log("Rock")
+
     return "Rock"
 }
 else if (random <= 6 && random > 3 ) {
-    console.log("Paper")
+  
     return "Paper"
 }
 else if (random <= 3) {
-    console.log("Scissors")
+
     return "Scissors"
 }
 
 
 }
 
-getComputerChoise()
+
+const computerSelection = getComputerChoice()
+const playerSelection = "Rock"
+
+
+
+function playRound (playerSelection, computerSelection)
+{ 
+playerSelection = playerSelection.toLowerCase()
+computerSelection = computerSelection.toLowerCase()
+console.log(playerSelection, computerSelection)
+
+if (playerSelection === computerSelection)
+ {
+console.log(`Draw! ${playerSelection} ties with ${computerSelection}`)
+return `Draw! ${playerSelection} ties with ${computerSelection}`
+ }
+
+else if (playerSelection == playerSelection.match(/rock/ig)
+     && computerSelection == computerSelection.match(/paper/ig)) {
+    console.log(`You Lose! ${computerSelection} beats ${playerSelection}`)
+}  
+
+else if (playerSelection == playerSelection.match(/paper/ig)
+     && computerSelection == computerSelection.match(/scissors/ig)) {
+    console.log(`You Lose! ${computerSelection} beats ${playerSelection}`)
+} 
+
+else if (playerSelection == playerSelection.match(/scissors/ig)
+     && computerSelection == computerSelection.match(/rock/ig)) {
+    console.log(`You Lose! ${computerSelection} beats ${playerSelection}`)
+} 
+
+else {
+    console.log(`You Win! ${playerSelection} beats ${computerSelection}`)
+}
+
+}
+
+playRound(playerSelection, computerSelection)
